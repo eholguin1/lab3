0daaf7e779d317c5b30c382f73a010b31630a7dc
eholguin1 <eholguin@ucsd.edu>
2023-04-20 13:00:28 -0700

    Update ArrayTests.java

--- ArrayTests.java
+++ ArrayTests.java
@@ -14,5 +14,16 @@ public class ArrayTests {
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+
+    int[] input2 = {10,15};
+    assertArrayEquals(new int[]{15,10}, ArrayExamples.reversed(input2));
   }
-}
+
+  @Test
+  public void testReverseInPlace2(){
+    int[] input = {10 ,15, 20};
+    ArrayExamples.reverseInPlace(input);
+    assertArrayEquals(new int[] {20, 15, 10}, input);
+  }
+
+ }
